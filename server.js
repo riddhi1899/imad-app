848bdb6c0fbdbdc92c63bd7b305664a1fa37dd4b
riddhi1899 <30663588+riddhi1899@users.noreply.github.com>
2018-02-16 22:10:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function(req, res) {
-    res send(createTemplate(ArticleOne));
+    res.send(createTemplate(ArticleOne));
 });
 app.get('/article-two',function(req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
